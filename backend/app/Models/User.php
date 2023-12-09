864fb7900e88b58f32c2d782812dd17bdf0a2e75
Samer Charaf Eddine <samer.charaf3@gmail.com>
2023-12-09 20:07:03 +0200

    Added the models plus the relation between them

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -23,6 +23,10 @@ class User extends Authenticatable
         'password',
     ];
 
+    public function orders(){
+        return $this->hasMany(Order::class);
+    }
+
     /**
      * The attributes that should be hidden for serialization.
      *
