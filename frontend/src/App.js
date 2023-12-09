577d674d63fde1eef66eeee54bc7313f922c5a12
Samer Charaf Eddine <samer.charaf3@gmail.com>
2023-12-09 17:07:56 +0200

    fixed frontend issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-import ProductCard from './Components/ProductCard';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import ProductCard from "./Components/ProductCard";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [products, setproducts] = useState([])
+  const [products, setproducts] = useState([]);
   function get_products() {
     let data = new FormData();
-    data.append('name',)
+
     axios({
       url: "http://127.0.0.1:8000/api/products",
       method: "get",
     }).then(function (res) {
-
-      setproducts(res.data.products)
-    })
+      setproducts(res.data.products);
+    });
   }
 
   useEffect(() => {
-    get_products()
-  }, [])
+    get_products();
+  }, []);
 
   return (
-    <div className='card-container'>
-      {products && (products.map((product) => {
-        return (<ProductCard key={product.id} title={product.title} description={product.description} />)
-      }))}
-
+    <div className="card-container">
+      {products &&
+        products.map((product) => {
+          return (
+            <ProductCard
+              key={product.id}
+              title={product.title}
+              description={product.description}
+            />
+          );
+        })}
     </div>
   );
 }
